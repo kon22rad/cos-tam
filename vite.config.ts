61c97179cf7e8b6c8283898fe1ccdf2ffc8068bc
Konrad <konrad.rajkowski@gmail.com>
2025-04-18 05:47:13 +0200

    Dodano konfigurację dla GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'; // Dodaj ten import na początku pliku
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cos-tam/', // Zaktualizuj nazwę repozytorium
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
